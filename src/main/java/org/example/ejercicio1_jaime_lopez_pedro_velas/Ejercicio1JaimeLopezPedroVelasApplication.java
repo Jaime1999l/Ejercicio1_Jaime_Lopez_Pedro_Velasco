14dc6f5fc2e8e1f0604897885a976ed712c43a71
Jaime López <114154176+Jaime1999l@users.noreply.github.com>
2024-09-16 10:53:57 +0200

    -

---
+++ src/main/java/org/example/ejercicio1_jaime_lopez_pedro_velas/Ejercicio1JaimeLopezPedroVelasApplication.java
@@ -0,0 +1,13 @@
+package org.example.ejercicio1_jaime_lopez_pedro_velas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio1JaimeLopezPedroVelasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ejercicio1JaimeLopezPedroVelasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ejercicio1_jaime_lopez_pedro_velas/Ejercicio1JaimeLopezPedroVelasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ejercicio1_jaime_lopez_pedro_velas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio1JaimeLopezPedroVelasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
